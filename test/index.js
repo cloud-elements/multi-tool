f624ba3c1e6e012ce2224631da0e0e1b90083dfd
Rocky Madden <git@rockymadden.com>
2017-02-03 15:58:30 -0700

    Bump coverage to 100%

--- test/index.js
+++ test/index.js
@@ -3,20 +3,44 @@
 const test = require('ava');
 const multitool = require('../src');
 
-test('installing a valid package with an exact version should return new package name', async t => {
-  const install0 = await multitool('node_modules', 'ramda', '0.23.0');
-  const install1 = await multitool('node_modules', 'ramda-fantasy', '0.7.0');
-
-  t.is(install0, 'ramda@0.23.0');
-  t.is(install1, 'ramda-fantasy@0.7.0');
-});
-
 test('installing a valid package with an exact version should work', async t => {
-  await multitool('node_modules', 'ramda', '0.23.0');
-  await multitool('node_modules', 'ramda-fantasy', '0.7.0');
+  const installed0 = await multitool('node_modules', 'ramda', '0.23.0');
+  const installed1 = await multitool('node_modules', 'ramda-fantasy', '0.7.0');
   const {identity} = require('ramda@0.23.0');
   const {Maybe} = require('ramda-fantasy@0.7.0');
 
+  t.is(installed0, 'ramda@0.23.0');
+  t.is(installed1, 'ramda-fantasy@0.7.0');
   t.is(identity('hello'), 'hello');
   t.true(Maybe.isNothing(Maybe.Nothing()));
+});
+
+test('installing a valid package with an x-based query should work', async t => {
+  const installed0 = await multitool('node_modules', 'ramda', '0.23.x');
+  const installed1 = await multitool('node_modules', 'ramda-fantasy', '0.x');
+  const {identity} = require('ramda@0.23.x');
+  const {Maybe} = require('ramda-fantasy@0.x');
+
+  t.is(installed0, 'ramda@0.23.x');
+  t.is(installed1, 'ramda-fantasy@0.x');
+  t.is(identity('hello'), 'hello');
+  t.true(Maybe.isNothing(Maybe.Nothing()));
+});
+
+test('installing an non-existent package should return null', async t => {
+  const installed = await multitool('node_modules', 'doesnt-exist', '0.0.0');
+
+  t.is(installed, null);
+});
+
+test('installing an non-existent package version should return null', async t => {
+  const installed = await multitool('node_modules', 'ramda', '99.99.99');
+
+  t.is(installed, null);
+});
+
+test('installing an invalidly named package should return null', async t => {
+  const installed = await multitool('node_modules', 'ramda', '>=99.99.99');
+
+  t.is(installed, null);
 });

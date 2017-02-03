6e0538d23c43bde98a4067b7f073921f1ed27928
Rocky Madden <git@rockymadden.com>
2017-02-03 14:13:42 -0700

    Baseline implementation

--- src/index.js
+++ src/index.js
@@ -1,8 +1,31 @@
 'use strict';
 
-const path = require('path');
+const pth = require('path');
+const execa = require('execa');
+const mkdirp = require('mkdirp');
 const pify = require('pify');
+const writeFile = require('write-file-atomic');
+const writePkg = require('write-pkg');
 
 module.exports = async (path, name, version) => {
+  const fullname = `${name}-${version}`;
+  const dir = pth.join(path, fullname);
+  const pkg = pth.join(dir, 'package.json');
+  const idx = pth.join(dir, 'index.js');
 
+  try {
+    await pify(mkdirp)(dir);
+    await writePkg(pkg, {
+      name: fullname,
+      version: '0.0.0',
+      main: 'index.js',
+      dependencies: {[name]: version}
+    });
+    await pify(writeFile)(idx, `module.exports = require('${name}');`);
+    await execa.shell(`cd ${dir} && npm install`);
+
+    return fullname;
+  } catch (err) {
+    return null;
+  }
 };
--- test/index.js
+++ test/index.js
@@ -1,1 +1,10 @@
 'use strict';
+
+const test = require('ava');
+const multitool = require('../src');
+
+test(async t => {
+  const install = await multitool('node_modules', 'ramda', '0.23.0');
+
+  t.is(install, 'ramda-0.23.0');
+});

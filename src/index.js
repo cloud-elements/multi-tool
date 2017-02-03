b2525119c91703e7a2859ffda7a1e9c7b2f56355
Rocky Madden <git@rockymadden.com>
2017-02-03 14:17:13 -0700

    Tweak code formatting

--- src/index.js
+++ src/index.js
@@ -12,16 +12,18 @@ module.exports = async (path, name, version) => {
   const dir = pth.join(path, fullname);
   const pkg = pth.join(dir, 'package.json');
   const idx = pth.join(dir, 'index.js');
+  const pkgContents = {
+    name: fullname,
+    version: '0.0.0',
+    main: 'index.js',
+    dependencies: {[name]: version}
+  };
+  const idxContents = `module.exports = require('${name}');`;
 
   try {
     await pify(mkdirp)(dir);
-    await writePkg(pkg, {
-      name: fullname,
-      version: '0.0.0',
-      main: 'index.js',
-      dependencies: {[name]: version}
-    });
-    await pify(writeFile)(idx, `module.exports = require('${name}');`);
+    await writePkg(pkg, pkgContents);
+    await pify(writeFile)(idx, idxContents);
     await execa.shell(`cd ${dir} && npm install`);
 
     return fullname;
--- test/index.js
+++ test/index.js
@@ -3,7 +3,7 @@
 const test = require('ava');
 const multitool = require('../src');
 
-test(async t => {
+test('installing a valid package with an exact version should return new package name', async t => {
   const install = await multitool('node_modules', 'ramda', '0.23.0');
 
   t.is(install, 'ramda-0.23.0');

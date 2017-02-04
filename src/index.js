7a52e58376064b0680f407f4997df98e1f760243
Rocky Madden <git@rockymadden.com>
2017-02-03 19:22:09 -0700

    Refactor to single HoF

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const pify = require('pify');
 const validFilename = require('valid-filename');
 const writeFile = require('write-file-atomic');
 
-const installSync = (path, name, version) => {
+const install = (sync, mkdir, mkfile, shell) => (path, name, version) => {
   if (!validFilename(`${name}@${version}`)) {
     return null;
   }
@@ -23,45 +23,32 @@ const installSync = (path, name, version) => {
   });
   const jsContents = `module.exports = require('${name}');`;
 
-  try {
-    mkdirp.sync(dir);
-    writeFile.sync(pkg, pkgContents);
-    writeFile.sync(js, jsContents);
-    execa.shellSync(`cd '${dir}' && npm install`);
+  if (sync) {
+    try {
+      mkdirp(dir);
+      mkfile(pkg, pkgContents);
+      mkfile(js, jsContents);
+      shell(`cd '${dir}' && npm install`);
 
-    return `${name}@${version}`;
-  } catch (err) {
-    return null;
+      return `${name}@${version}`;
+    } catch (err) {
+      return null;
+    }
+  } else {
+    return (async () => {
+      try {
+        await mkdirp(dir);
+        await mkfile(pkg, pkgContents);
+        await mkfile(js, jsContents);
+        await shell(`cd '${dir}' && npm install`);
+
+        return `${name}@${version}`;
+      } catch (err) {
+        return null;
+      }
+    })();
   }
 };
 
-const installAsync = async (path, name, version) => {
-  if (!validFilename(`${name}@${version}`)) {
-    return null;
-  }
-
-  const dir = pth.join(path, `${name}@${version}`);
-  const pkg = pth.join(dir, 'package.json');
-  const js = pth.join(dir, 'index.js');
-  const pkgContents = JSON.stringify({
-    name: `${name}-${version}`,
-    version: '0.0.0',
-    main: 'index.js',
-    dependencies: {[name]: version}
-  });
-  const jsContents = `module.exports = require('${name}');`;
-
-  try {
-    await pify(mkdirp)(dir);
-    await pify(writeFile)(pkg, pkgContents);
-    await pify(writeFile)(js, jsContents);
-    await execa.shell(`cd '${dir}' && npm install`);
-
-    return `${name}@${version}`;
-  } catch (err) {
-    return null;
-  }
-};
-
-module.exports = installAsync;
-module.exports.sync = installSync;
+module.exports = install(false, pify(mkdirp), pify(writeFile), execa.shell);
+module.exports.sync = install(true, mkdirp.sync, writeFile.sync, execa.shellSync);

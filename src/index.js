9a7fc77b5213206d06e1bddbf4ead28d12f5e126
Rocky Madden <git@rockymadden.com>
2017-02-06 09:25:22 -0700

    Tweak variable name

--- src/index.js
+++ src/index.js
@@ -17,21 +17,21 @@ const install = async (path, name, version) => {
   }
 
   const dir = pth.join(path, `${name}@${version}`);
-  const pkg = pth.join(dir, 'package.json');
+  const pkgPath = pth.join(dir, 'package.json');
   const pkgContents = JSON.stringify({
     name: `${name}-${version}`,
     version: '0.0.0',
     main: 'index.js',
     dependencies: {[name]: version}
   });
-  const js = pth.join(dir, 'index.js');
+  const jsPath = pth.join(dir, 'index.js');
   const jsContents = `module.exports = require('${name}');`;
 
   try {
     await rmdir(dir);
     await mkdir(dir);
-    await write(pkg, pkgContents);
-    await write(js, jsContents);
+    await write(pkgPath, pkgContents);
+    await write(jsPath, jsContents);
     await shell(`cd '${dir}' && npm install`);
 
     return `${name}@${version}`;

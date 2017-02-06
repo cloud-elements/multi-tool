33d0082d5801c60a2621d30b91dfba673413830d
Rocky Madden <git@rockymadden.com>
2017-02-06 07:52:43 -0700

    Overwrite and clean up upon error

--- src/index.js
+++ src/index.js
@@ -2,12 +2,11 @@
 
 const pth = require('path');
 const execa = require('execa');
-const mkdirp = require('mkdirp');
+const fs = require('fs-extra');
 const pify = require('pify');
 const validFilename = require('valid-filename');
-const writeFile = require('write-file-atomic');
 
-const install = (sync, mkdir, mkfile, shell) => (path, name, version) => {
+const install = sync => (mkdir, mkfile, rmdir, shell) => (path, name, version) => {
   if (!validFilename(`${name}@${version}`)) {
     return null;
   }
@@ -25,30 +24,40 @@ const install = (sync, mkdir, mkfile, shell) => (path, name, version) => {
 
   if (sync) {
     try {
-      mkdirp(dir);
+      rmdir(dir);
+      mkdir(dir);
       mkfile(pkg, pkgContents);
       mkfile(js, jsContents);
       shell(`cd '${dir}' && npm install`);
 
       return `${name}@${version}`;
     } catch (err) {
+      try {
+        rmdir(dir);
+      } catch (err) { }
+
       return null;
     }
   } else {
     return (async () => {
       try {
-        await mkdirp(dir);
+        await rmdir(dir);
+        await mkdir(dir);
         await mkfile(pkg, pkgContents);
         await mkfile(js, jsContents);
         await shell(`cd '${dir}' && npm install`);
 
         return `${name}@${version}`;
       } catch (err) {
+        try {
+          rmdir(dir);
+        } catch (err) { }
+
         return null;
       }
     })();
   }
 };
 
-module.exports = install(false, pify(mkdirp), pify(writeFile), execa.shell);
-module.exports.sync = install(true, mkdirp.sync, writeFile.sync, execa.shellSync);
+module.exports = install(false)(pify(fs.mkdirp), pify(fs.outputFile), pify(fs.remove), execa.shell);
+module.exports.sync = install(true)(fs.mkdirpSync, fs.outputFileSync, fs.removeSync, execa.shellSync);

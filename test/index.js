d4c24f1de7e2b42423c0642094dd973366562bda
Rocky Madden <git@rockymadden.com>
2017-02-06 07:59:15 -0700

    Execute tests serially

--- test/index.js
+++ test/index.js
@@ -3,7 +3,7 @@
 const test = require('ava');
 const multitool = require('../src');
 
-test('installing a valid package with an exact version should work', async t => {
+test.serial('installing a valid package with an exact version should work', async t => {
   const installed0 = await multitool('node_modules', 'ramda', '0.23.0');
   const installed1 = await multitool('node_modules', 'ramda-fantasy', '0.7.0');
   const {identity} = require('ramda@0.23.0');
@@ -15,7 +15,7 @@ test('installing a valid package with an exact version should work', async t => 
   t.true(Maybe.isNothing(Maybe.Nothing()));
 });
 
-test('synchronously installing a valid package with an exact version should work', t => {
+test.serial('synchronously installing a valid package with an exact version should work', t => {
   const installed0 = multitool.sync('node_modules', 'ramda', '0.23.0');
   const installed1 = multitool.sync('node_modules', 'ramda-fantasy', '0.7.0');
   const {identity} = require('ramda@0.23.0');
@@ -27,7 +27,7 @@ test('synchronously installing a valid package with an exact version should work
   t.true(Maybe.isNothing(Maybe.Nothing()));
 });
 
-test('installing a valid package with an x-based query should work', async t => {
+test.serial('installing a valid package with an x-based query should work', async t => {
   const installed0 = await multitool('node_modules', 'ramda', '0.23.x');
   const installed1 = await multitool('node_modules', 'ramda-fantasy', '0.x');
   const {identity} = require('ramda@0.23.x');
@@ -39,7 +39,7 @@ test('installing a valid package with an x-based query should work', async t => 
   t.true(Maybe.isNothing(Maybe.Nothing()));
 });
 
-test('synchronously installing a valid package with an x-based query should work', t => {
+test.serial('synchronously installing a valid package with an x-based query should work', t => {
   const installed0 = multitool.sync('node_modules', 'ramda', '0.23.x');
   const installed1 = multitool.sync('node_modules', 'ramda-fantasy', '0.x');
   const {identity} = require('ramda@0.23.x');
@@ -51,37 +51,37 @@ test('synchronously installing a valid package with an x-based query should work
   t.true(Maybe.isNothing(Maybe.Nothing()));
 });
 
-test('installing an non-existent package should return null', async t => {
+test.serial('installing an non-existent package should return null', async t => {
   const installed = await multitool('node_modules', 'doesnt-exist', '0.0.0');
 
   t.is(installed, null);
 });
 
-test('synchronously installing an non-existent package should return null', t => {
+test.serial('synchronously installing an non-existent package should return null', t => {
   const installed = multitool.sync('node_modules', 'doesnt-exist', '0.0.0');
 
   t.is(installed, null);
 });
 
-test('installing an non-existent package version should return null', async t => {
+test.serial('installing an non-existent package version should return null', async t => {
   const installed = await multitool('node_modules', 'ramda', '99.99.99');
 
   t.is(installed, null);
 });
 
-test('synchronously installing an non-existent package version should return null', t => {
+test.serial('synchronously installing an non-existent package version should return null', t => {
   const installed = multitool.sync('node_modules', 'ramda', '99.99.99');
 
   t.is(installed, null);
 });
 
-test('installing an invalidly named package should return null', async t => {
+test.serial('installing an invalidly named package should return null', async t => {
   const installed = await multitool('node_modules', 'ramda', '>=99.99.99');
 
   t.is(installed, null);
 });
 
-test('synchronously installing an invalidly named package should return null', t => {
+test.serial('synchronously installing an invalidly named package should return null', t => {
   const installed = multitool.sync('node_modules', 'ramda', '>=99.99.99');
 
   t.is(installed, null);

25f790fbf3bfe35133b96a8d10b672eb9989c7fa
Rocky Madden <git@rockymadden.com>
2017-04-04 13:42:51 -0600

    Prune prior to suite run

--- test.js
+++ test.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const test = require('ava');
+const shell = require('execa');
 const {filter, identity, map} = require('ramda');
 const {create, env} = require('sanctuary');
 const multi = require('.');
@@ -12,6 +13,8 @@ const never = (name, version, ago) => ago >= Number.MAX_SAFE_INTEGER;
 const installDefault = multi({path: 'node_modules'});
 const installAlways = multi({delay: 10000, path: 'node_modules', invalidate: always, timeout: 25000});
 const installNever = multi({delay: 10000, path: 'node_modules', invalidate: never, timeout: 25000});
+
+test.before(() => shell('npm', ['prune']));
 
 test('installing a valid package with latest version should return Right', async t => {
 	const install = fromEither({}, await installDefault('ramda', 'latest'));
